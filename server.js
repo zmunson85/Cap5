f226715fe77b364e5f42e1e666bfbfbe33df78bb
Zach Munson <zmunson85@gmail.com>
2021-12-13 21:49:11 -0700

    'Connected-MongoDB'

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 const express = require('express');
+const connectDB = require('./config/db')
 
 const app = express();
 
-app.get('/', (req, res) => res.json({ msg: 'My Contact Api' }));
 
-//app routes
+//connect the Database
+connectDB();
+
+app.get('/', (req, res) =>
+    res.json({ msg: 'My Contact Api' }));
+
+//api routes
 app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/contacts', require('./routes/contacts'));
