102c2086798edb8564264e30dd69b3decb15f0ac
Zach Munson <zmunson85@gmail.com>
2021-12-16 11:39:29 -0700

    'Corrected_Server.js'

--- server.js
+++ server.js
@@ -10,9 +10,6 @@ connectDB();
 //middleware
 app.use(express.json({ extended: false }));
 
-app.get('/', (req, res) =>
-    res.json({ msg: 'My Contact Api' }));
-
 //api routes
 app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
